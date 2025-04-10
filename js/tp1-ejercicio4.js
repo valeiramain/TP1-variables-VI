45a170a060336492dff2ee3399e21d4828a1ecd9
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-10 10:22:20 -0300

    Se realizan ejercicios 4 y 5. Se agrega archivo readme

---
+++ js/tp1-ejercicio4.js
@@ -0,0 +1,3 @@
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+const nombreUsuario = prompt("Ingrese nombre del Usuario: ")
+document.writeln("Hola "+nombreUsuario)
---
+++ js/tp1-ejercicio5.js
@@ -0,0 +1,4 @@
+//5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+const numero1 = parseInt(prompt("Ingrese primer número: "));
+const numero2 = parseInt(prompt("Ingrese segundo número: "));
+document.writeln("La suma de ambos numeros es = "+(numero1+numero2))
