0a59109d552fd3cd40e239cd3353aef85400e783
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-11 12:49:19 -0300

    Se realiza elercicio 9

--- js/tp1-ejercicio9.js
+++ js/tp1-ejercicio9.js
@@ -1,2 +1,26 @@
 //9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+const frase = prompt("Escriba una Frase: ");
+const fraseMayusculas =  frase.toUpperCase();
+console.log(frase)
+console.log(fraseMayusculas)
+console.log(frase.length)
+
+for (let i=0; i<= fraseMayusculas.length; i++) {
+    if ((fraseMayusculas.charAt(i))=="A"){
+        document.writeln(fraseMayusculas.charAt(i));
+    }
+    if ((fraseMayusculas.charAt(i))=="E"){
+        document.writeln(fraseMayusculas.charAt(i));
+    }
+    if ((fraseMayusculas.charAt(i))=="I"){
+        document.writeln(fraseMayusculas.charAt(i));
+    }
+    if ((fraseMayusculas.charAt(i))=="O"){
+        document.writeln(fraseMayusculas.charAt(i));
+    }
+    if ((fraseMayusculas.charAt(i))=="U"){
+        document.writeln(fraseMayusculas.charAt(i));
+    }
+}
