16bf3250850fffbb0de2db06e923422d24cc0dba
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-14 11:38:28 -0300

    corrijo ejercicio 9 para que se usen sólo condicionales, y se restringe cantidad maxima de caracteres a ingresar

--- js/tp1-ejercicio9.js
+++ js/tp1-ejercicio9.js
@@ -1,26 +1,34 @@
 //9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 
-const frase = prompt("Escriba una Frase: ");
-const fraseMayusculas =  frase.toUpperCase();
-console.log(frase)
-console.log(fraseMayusculas)
-console.log(frase.length)
+const frase = prompt("Ingrese una frase de hasta 5 caracteres: ").toLowerCase();
 
-for (let i=0; i<= fraseMayusculas.length; i++) {
-    if ((fraseMayusculas.charAt(i))=="A"){
-        document.writeln(fraseMayusculas.charAt(i));
+console.log(frase)
+console.log(frase.length)
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(frase.charAt(0))
+
+if (!NaN(frase)) {
+    if (frase.length <= 5) {
+        if (frase.charAt(0) === "a" || frase.charAt(0) === "e" || frase.charAt(0) === "i" || frase.charAt(0) === "o" || frase.charAt(0) === "u") {
+            document.writeln(frase.charAt(0))
+        }
+        if (frase.charAt(1) === "a" || frase.charAt(1) === "e" || frase.charAt(1) === "i" || frase.charAt(1) === "o" || frase.charAt(1) === "u") {
+            document.writeln(frase.charAt(1))
+        }
+        if (frase.charAt(2) === "a" || frase.charAt(2) === "e" || frase.charAt(2) === "i" || frase.charAt(2) === "o" || frase.charAt(2) === "u") {
+            document.writeln(frase.charAt(2))
+        }
+        if (frase.charAt(3) === "a" || frase.charAt(3) === "e" || frase.charAt(3) === "i" || frase.charAt(3) === "o" || frase.charAt(3) === "u") {
+            document.writeln(frase.charAt(3))
+        }
+        if (frase.charAt(4) === "a" || frase.charAt(4) === "e" || frase.charAt(4) === "i" || frase.charAt(4) === "o" || frase.charAt(4) === "u") {
+            document.writeln(frase.charAt(4))
+        }
+    } else {
+        alert("Ingresaste una cadena inválida")
     }
-    if ((fraseMayusculas.charAt(i))=="E"){
-        document.writeln(fraseMayusculas.charAt(i));
-    }
-    if ((fraseMayusculas.charAt(i))=="I"){
-        document.writeln(fraseMayusculas.charAt(i));
-    }
-    if ((fraseMayusculas.charAt(i))=="O"){
-        document.writeln(fraseMayusculas.charAt(i));
-    }
-    if ((fraseMayusculas.charAt(i))=="U"){
-        document.writeln(fraseMayusculas.charAt(i));
-    }
+} else {
+    alert("Ingresaste una número")
 }
