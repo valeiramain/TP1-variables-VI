f34efccd50ebb8ec30f02ff671d7047ffbfce04b
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-14 11:42:56 -0300

    Se corrige ejercicio9

--- js/tp1-ejercicio9.js
+++ js/tp1-ejercicio9.js
@@ -4,31 +4,29 @@
 const frase = prompt("Ingrese una frase de hasta 5 caracteres: ").toLowerCase();
 
 console.log(frase)
+console.log(NaN(frase))
 console.log(frase.length)
 console.log(frase.toUpperCase())
 console.log(frase.toLowerCase())
 console.log(frase.charAt(0))
 
-if (!NaN(frase)) {
-    if (frase.length <= 5) {
-        if (frase.charAt(0) === "a" || frase.charAt(0) === "e" || frase.charAt(0) === "i" || frase.charAt(0) === "o" || frase.charAt(0) === "u") {
-            document.writeln(frase.charAt(0))
-        }
-        if (frase.charAt(1) === "a" || frase.charAt(1) === "e" || frase.charAt(1) === "i" || frase.charAt(1) === "o" || frase.charAt(1) === "u") {
-            document.writeln(frase.charAt(1))
-        }
-        if (frase.charAt(2) === "a" || frase.charAt(2) === "e" || frase.charAt(2) === "i" || frase.charAt(2) === "o" || frase.charAt(2) === "u") {
-            document.writeln(frase.charAt(2))
-        }
-        if (frase.charAt(3) === "a" || frase.charAt(3) === "e" || frase.charAt(3) === "i" || frase.charAt(3) === "o" || frase.charAt(3) === "u") {
-            document.writeln(frase.charAt(3))
-        }
-        if (frase.charAt(4) === "a" || frase.charAt(4) === "e" || frase.charAt(4) === "i" || frase.charAt(4) === "o" || frase.charAt(4) === "u") {
-            document.writeln(frase.charAt(4))
-        }
-    } else {
-        alert("Ingresaste una cadena inválida")
+
+if (frase.length <= 5) {
+    if (frase.charAt(0) === "a" || frase.charAt(0) === "e" || frase.charAt(0) === "i" || frase.charAt(0) === "o" || frase.charAt(0) === "u") {
+        document.writeln(frase.charAt(0))
+    }
+    if (frase.charAt(1) === "a" || frase.charAt(1) === "e" || frase.charAt(1) === "i" || frase.charAt(1) === "o" || frase.charAt(1) === "u") {
+        document.writeln(frase.charAt(1))
+    }
+    if (frase.charAt(2) === "a" || frase.charAt(2) === "e" || frase.charAt(2) === "i" || frase.charAt(2) === "o" || frase.charAt(2) === "u") {
+        document.writeln(frase.charAt(2))
+    }
+    if (frase.charAt(3) === "a" || frase.charAt(3) === "e" || frase.charAt(3) === "i" || frase.charAt(3) === "o" || frase.charAt(3) === "u") {
+        document.writeln(frase.charAt(3))
+    }
+    if (frase.charAt(4) === "a" || frase.charAt(4) === "e" || frase.charAt(4) === "i" || frase.charAt(4) === "o" || frase.charAt(4) === "u") {
+        document.writeln(frase.charAt(4))
     }
 } else {
-    alert("Ingresaste una número")
+    alert("Ingresaste una cadena inválida")
 }
